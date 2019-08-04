42c8a57cca575c19e24509a3f81dd6c35c82cdc0
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-08-04 09:39:48 -0500

    add search filter and auto-complete for search input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div v-for="product in products">
+    Search:
+    <input v-model="searchFilter" type="text" list="names" />
+    <datalist id="names">
+      <option v-for="product in products">{{ product.name }}</option>
+    </datalist>
+    <div v-for="product in filterBy(products, searchFilter, 'name', 'description')">
       <h2>{{ product.name }}</h2>
       <img v-bind:src="product.image_url" alt="" />
       <router-link v-bind:to="`/products/${product.id}`">More Info</router-link>
@@ -17,12 +22,15 @@ img {
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       message: "Welcome to Vue.js",
-      products: []
+      products: [],
+      searchFilter: ""
     };
   },
   created: function() {
